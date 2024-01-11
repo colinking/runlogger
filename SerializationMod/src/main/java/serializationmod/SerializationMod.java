4ca431e9169f6a6bc0ccab21719ce5cfbce0ec77
Colin <me@colinking.co>
2024-01-11 18:53:34 -0500

    reformat code

--- SerializationMod/src/main/java/serializationmod/SerializationMod.java
+++ SerializationMod/src/main/java/serializationmod/SerializationMod.java
@@ -16,61 +16,62 @@ import java.util.Properties;
 @SpireInitializer
 public class SerializationMod implements PostInitializeSubscriber {
 
-    private static final Logger logger = LogManager.getLogger(SerializationMod.class.getName());
-    private static final String MODNAME = "Serialization Mod";
-    private static final String AUTHOR = "Colin King";
-    private static final String DESCRIPTION = "Serializes the state of Slay the Spire runs.";
-    private static SpireConfig communicationConfig;
-    private static final String VERBOSE_OPTION = "verbose";
-    private static final boolean DEFAULT_VERBOSITY = false;
+	private static final Logger logger = LogManager.getLogger(SerializationMod.class.getName());
+	private static final String MODNAME = "Serialization Mod";
+	private static final String AUTHOR = "Colin King";
+	private static final String DESCRIPTION = "Serializes the state of Slay the Spire runs.";
+	private static SpireConfig communicationConfig;
+	private static final String VERBOSE_OPTION = "verbose";
+	private static final boolean DEFAULT_VERBOSITY = false;
 
-    public SerializationMod() {
-        BaseMod.subscribe(this);
-        try {
-            Properties defaults = new Properties();
-            defaults.put(VERBOSE_OPTION, Boolean.toString(DEFAULT_VERBOSITY));
-            communicationConfig = new SpireConfig("SerializationMod", "config", defaults);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+	public SerializationMod() {
+		BaseMod.subscribe(this);
+		try {
+			Properties defaults = new Properties();
+			defaults.put(VERBOSE_OPTION, Boolean.toString(DEFAULT_VERBOSITY));
+			communicationConfig = new SpireConfig("SerializationMod", "config", defaults);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
-//    TODO(colin): unused?
-    public static void initialize() {
-        SerializationMod mod = new SerializationMod();
-    }
+	// TODO(colin): unused?
+	public static void initialize() {
+		SerializationMod mod = new SerializationMod();
 
-    public void receivePostInitialize() {
-        setUpOptionsMenu();
-    }
+	}
 
-    private void setUpOptionsMenu() {
-        ModPanel settingsPanel = new ModPanel();
+	public void receivePostInitialize() {
+		setUpOptionsMenu();
+	}
 
-        ModLabeledToggleButton verbosityOption = new ModLabeledToggleButton(
-                "Show verbose log output",
-                350, 500, Settings.CREAM_COLOR, FontHelper.charDescFont,
-                getVerbosityOption(), settingsPanel, modLabel -> {
-        },
-                modToggleButton -> {
-                    if (communicationConfig != null) {
-                        communicationConfig.setBool(VERBOSE_OPTION, modToggleButton.enabled);
-                        try {
-                            communicationConfig.save();
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                });
-        settingsPanel.addUIElement(verbosityOption);
+	private void setUpOptionsMenu() {
+		ModPanel settingsPanel = new ModPanel();
 
-        BaseMod.registerModBadge(ImageMaster.loadImage("Icon.png"), MODNAME, AUTHOR, DESCRIPTION, settingsPanel);
-    }
+		ModLabeledToggleButton verbosityOption = new ModLabeledToggleButton(
+			"Show verbose log output",
+			350, 500, Settings.CREAM_COLOR, FontHelper.charDescFont,
+			getVerbosityOption(), settingsPanel, modLabel -> {
+		},
+			modToggleButton -> {
+				if (communicationConfig != null) {
+					communicationConfig.setBool(VERBOSE_OPTION, modToggleButton.enabled);
+					try {
+						communicationConfig.save();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+			});
+		settingsPanel.addUIElement(verbosityOption);
 
-    private static boolean getVerbosityOption() {
-        if (communicationConfig == null) {
-            return DEFAULT_VERBOSITY;
-        }
-        return communicationConfig.getBool(VERBOSE_OPTION);
-    }
+		BaseMod.registerModBadge(ImageMaster.loadImage("Icon.png"), MODNAME, AUTHOR, DESCRIPTION, settingsPanel);
+	}
+
+	private static boolean getVerbosityOption() {
+		if (communicationConfig == null) {
+			return DEFAULT_VERBOSITY;
+		}
+		return communicationConfig.getBool(VERBOSE_OPTION);
+	}
 }
